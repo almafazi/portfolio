941beb68da093c2b1d56be233020339b014776da
Felipe Vieira <felipe.svfx@gmail.com>
2021-01-26 20:39:09 -0300

    Init project #portfolio

---
+++ src/pages/teste.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        justifyContent: 'center',
+        display: 'flex',
+        alignItems: 'center',
+        height: '100vh'
+      }}
+    >
+      <h1>OLá</h1>
+    </div>
+  )
+}
